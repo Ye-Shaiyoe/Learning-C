4975720fe944ee80c10bddbb028aa085e7b7c34c
Akrom <akrommuhammadyusuf@gmail.com>
2025-12-24 15:10:44 +0700

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Arch Linux dengan VS Code!\n");
+    return 0;
+}
---
+++ perulangan.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int i = 10; 
+    while (i < 10000000) {
+        printf ("ini adalah yang ke : %d\n", i);
+        i++;
+    }
+    return 0;
+}
