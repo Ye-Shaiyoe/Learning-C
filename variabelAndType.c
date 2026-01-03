6e1e7611b8225c798b360d90cbefd7175d895c33
krom~ <akrommuhammadyusuf@gmail.com>
2026-01-03 23:50:15 +0700

    Add files via upload
    Signed-off-by: krom~ <akrommuhammadyusuf@gmail.com>

---
+++ variabelAndType.c
@@ -0,0 +1,13 @@
+/* its eazy to learn C programming language right? */
+
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    float b = 4.5;
+    double c = 3.14159;
+    char huruf = 'A';
+    float sum = a + b + c;
+    printf("The sum of %d, %.2f, and %.5f is %.5f, and thats %c\n", a, b, c, sum, huruf);
+    return 0;
+}
